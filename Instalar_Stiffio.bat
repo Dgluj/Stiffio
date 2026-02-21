cab1511586512b7be30291a4e4c9a8b3ca855e61
catajonqui <catajonqui@gmail.com>
2026-02-21 20:42:27 -0300

    detalle .exe

--- Instalar_Stiffio.bat
+++ Instalar_Stiffio.bat
@@ -1,13 +1,13 @@
 @echo off
-set target=%~dp0dist\Stiffio App.exe
+set target=%~dp0dist\StiffioApp.exe
 
 for /f "delims=" %%i in ('powershell -command "[Environment]::GetFolderPath('Desktop')"') do set desktop=%%i
 
-set shortcut=%desktop%\Stiffio App.lnk
+set shortcut=%desktop%\StiffioApp.lnk
 
 powershell ^
 $s=(New-Object -COM WScript.Shell).CreateShortcut('%shortcut%'); ^
 $s.TargetPath='%target%'; ^
 $s.WorkingDirectory='%~dp0dist'; ^
-$s.IconLocation='%target%,0'; ^
+$s.IconLocation='%target%'; ^
 $s.Save()
